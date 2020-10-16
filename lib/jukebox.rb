e84fdeedc7fea4a0d02dca6f00dc39052d78f944
Michael Kim <m.kim4247@gmail.com>
2020-10-16 01:47:14 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,54 +10,45 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
-def help 
-  puts "I accept the following commands:
-    - help : displays this help message
-    - list : displays a list of songs you can play
-    - play : lets you choose a song to play
-    - exit : exits this program"
-end 
- 
-def list(songs)
-  songs.each_with_index do |song, index|
-    puts "#{index + 1}. #{song}"
-  end 
-end 
+def help
+puts 'I accept the following commands:'
+puts '- help : displays this help message'
+puts '- list : displays a list of songs you can play'
+puts '- play : lets you choose a song to play'
+puts '- exit : exits this program'
+end
 
 def play(songs)
   puts "Please enter a song name or number:"
-  users_input = gets.chomp 
-  
-  songs.each_with_index do |song, index|
-    if song.include?(users_input)
-      puts "Playing #{song}"
-    else 
-      puts "Invalid input, please try again"
-    end 
-  end 
-end 
+  user_response = gets.strip
+  if songs.include?(user_response)
+    puts "Playing #{user_response}"
+  else
+    puts "Invalid input, please try again"
+end
 
-def exit_jukebox 
+def list(songs)
+  songs.each_with_index do |item, index|
+    puts "#{index + 1}. #{item}"
+end
+
+def exit_jukebox
   puts "Goodbye"
-end 
+end
 
-def run(songs) 
-  help 
+def run
+  help
   puts "Please enter a command:"
-  users_input = gets.chomp 
-  
-    if users_input == "list"
+  user_response = gets.strip
+  while user_response != 'exit'
+    if user_response == 'play'
+      play(songs)
+    elsif user_response == 'list'
       list(songs)
-      run(songs)
-    elsif users_input == "play"
-      play(songs) 
-      run(songs)
-    elsif users_input == "help"
-      run(songs)
-    elsif users_input == "exit"
+    elsif user_response == 'help'
+      help
+    elsif user_response == 'exit'
       exit_jukebox
     end
-end 
-
-
-
+  end
+end
